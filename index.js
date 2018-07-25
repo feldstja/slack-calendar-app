1b17968b40d0aee353153294a452f553c84993fd
Jacob <jacob.a.feldstein@vanderbilt.edu>
2018-07-25 14:36:12 -0700

    can add dates to google

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ rtm.start();
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
 // See the "Combining with the WebClient" topic below for an example of how to get this ID
 // const web = new WebClient(slackToken);
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.REDIRECT_URL);
 
-rtm.on('message', (event)=>{
-  console.log(event)
-  const sessionPath = sessionClient.sessionPath(projectId, event.user);
-  const oAuth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.REDIRECT_URL);
+  rtm.on('message', (event)=>{
+    console.log(event)
+    const sessionPath = sessionClient.sessionPath(projectId, event.user);
 
     const request = {
       session: sessionPath,
@@ -52,106 +52,102 @@ rtm.on('message', (event)=>{
     // .then(resp => console.log(resp))
     .then(resp => {
       if (!resp){
-
         const authUrl = oAuth2Client.generateAuthUrl({
           access_type: 'offline',
           scope: SCOPES,
-          redirect_url: process.env.REDIRECT_URL
+          redirect_url: process.env.REDIRECT_URL,
+          state: JSON.stringify({userId: event.user, channelId: event.channel})
         });
         rtm.sendMessage(authUrl, event.channel);
         console.log(authUrl);
+        console.log("RESPONSE SHOULD NOT BE HERE:", resp)
 
-
-        app.get('/google/callback', (req, res)=>{
-          oAuth2Client.getToken(req.query.code, (err, token)=>{
-            console.log("TOKEN: ", token)
-
-            oAuth2Client.setCredentials(token);
-
-
-            const tokenMDB = new models.tokenMDB({
-              access: token.access_token,
-              refresh: token.refresh_token,
-              expiry: token.expiry_date,
-              slackId: event.user
-            })
-
-            tokenMDB.save()
-            .then(resp => {
-              if (resp.status === 200){
-                rtm.sendMessage('You have authorized us')
-              }
-            })
-
-
-
-          })
-        })
       } else{
-        oAuth2Client.setCredentials(resp.access);
+        console.log("RESPONSE RIGHT HERE:", resp)
+        oAuth2Client.setCredentials({
+          access_token: resp.access,
+          refresh_token: resp.refresh,
+        });
       }
       sendToDialogFlow(request)
       .then((response)=>{
-        console.log(response);
+        console.log("RESPONSE:", response);
         rtm.sendMessage(response.fulfillmentText, event.channel)
-        if(response.allRequiredParamsPresent){
+        if(response.allRequiredParamsPresent && response.action !== 'input.welcome'){
           oAuth2Client.on('token', (err, token) => {
+            console.log("TOKEN:", token)
             if (token.refresh_token){
-
+              console.log("TOKEN:", token)
             }
           })
           const calendar = google.calendar({version: 'v3', auth: oAuth2Client})
-          calendar.events.quickAdd({
-            calendarId: 'primary',
-            text: 'codealong at 3:00 pm today',
-            //   body: {
-            //   "end": {
-            //     dateTime: new Date(Date.now() + 90000)
-            //   },
-            //   "start": {
-            //     dateTime: new Date(Date.now() + 30000)
-            //   },
-            //   "summary": 'doing a codealong',
-            // }
-          }, (err, resp)=>{
-            console.log(err)
-          })
-          let date = response.parameters.fields.date
-          let action = response.parameters.fields.action
+          // var event = {
+          //
+          // };
+          console.log("ACTION:", response.parameters.fields.action.listValue.values[0].stringValue)
+          console.log("DATETIME:", response.parameters.fields.time.stringValue)
+
+          let d = new Date(response.parameters.fields.time.stringValue)
+          let e = new Date(d + (1000 * 60 * 30))
+
+          let dStr = e.toISOString();
+          console.log("EEEEE:", dStr)
+
+          var otherEvent = {
+            'summary': response.parameters.fields.action.listValue.values[0].stringValue,
+            'start': {
+              'dateTime': response.parameters.fields.time.stringValue,
+            },
+            'end': {
+              'dateTime': dStr,
+            },
+          };
+
+          calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': otherEvent
+          });
+
+
         }
       })
-
-
     })
   })
 
 
-
-
-
-  // Load the current channels list asynchrously
-  // web.channels.list()
-  // .then((res) => {
-  //   // Take any channel for which the bot is a member
-  //   const channel = res.channels.find(c => c.is_member);
-  //
-  //   if (channel) {
-  //     // We now have a channel ID to post a message in!
-  //     // use the `sendMessage()` method to send a simple string to a channel using the channel ID
-  //     rtm.sendMessage('Hello, world!', channel.id)
-  //       // Returns a promise that resolves when the message is sent
-  //       .then((msg) => console.log(`Message sent to channel ${channel.name} with ts:${msg.ts}`))
-  //       .catch(console.error);
-  //   } else {
-  //     console.log('This bot does not belong to any channel, invite it to at least one and try again');
-  //   }
-  // });
-
-
-
-
   // An access token (from your Slack app or custom integration - usually xoxb)
+  app.get('/google/callback', (req, res)=>{
+    oAuth2Client.getToken(req.query.code, (err, token)=>{
+      if(err){
+        console.log("HERES THE ERROR:", err)
+        return
+      }
 
+      console.log("TOKEN 2: ", token)
+      if(token){
+        console.log("TOKEN 3: ", token)
+        let obj = JSON.parse(req.query.state)
+        oAuth2Client.setCredentials(token);
+
+        console.log("TOKEN 4: ", token)
+
+        const tokenMDB = new models.tokenMDB({
+          access: token.access_token,
+          refresh: token.refresh_token,
+          expiry: token.expiry_date,
+          slackId: obj.userId
+        })
+        console.log("TOKEN 5: ", token)
+
+        tokenMDB.save()
+        .then(resp => {
+          console.log("TOKEN 6:", resp)
+          res.send('good job')
+          rtm.sendMessage('You have authorized us', obj.channelId)
+        })
+      }
+    })
+  })
 
   // Send request and log result
   function sendToDialogFlow(request){
@@ -173,5 +169,5 @@ rtm.on('message', (event)=>{
       console.error('ERROR:', err);
     });
   }
-  
+
   app.listen(1337);
